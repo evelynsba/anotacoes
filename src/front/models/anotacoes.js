e6a027a13e8c4771dc34f3bb6186e103aa9bd803
evelynsba <evelynsba@gmail.com>
2021-09-13 19:59:04 -0300

    criacao de pastas MVC e arquivo anotacoes js

---
+++ src/front/models/anotacoes.js
@@ -0,0 +1,15 @@
+
+class Anotacoes{
+    constructor(titulo, notas){
+        titulo = this.titulo;
+        notas = this.notas;
+    
+    try {
+        if(this.titulo == "" && this.notas == ""){
+
+        }
+    } catch (error) {
+        throw new error("Criacao de notas - dados invalidos")
+    }
+}
+}
